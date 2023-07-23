32e3dbd57c40891fe57d1f88354879dc77fdecc6
mostertjc <102650119+mostertjc@users.noreply.github.com>
2023-07-23 20:19:33 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    logging = !(logging)
+    if (logging) {
+        basic.showIcon(IconNames.Target)
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.UntilDone)
+    } else {
+        basic.clearScreen()
+    }
+})
+let logging = false
+logging = false
+loops.everyInterval(60000, function () {
+    if (logging) {
+        datalogger.log(
+        datalogger.createCV("temp", input.temperature()),
+        datalogger.createCV("light", input.lightLevel())
+        )
+    } else {
+    	
+    }
+})
